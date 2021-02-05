e6fe0d0ca368ac389208237beb5d8cc27dfcf200
Ivan Vieira <iivnn00@gmail.com>
2021-02-05 19:54:19 -0300

    bootstrap-static

--- index.js
+++ index.js
@@ -12,16 +12,22 @@ const PORT = 6565
     const app = express()
     app.use(bodyparser.urlencoded({extended : true}))
     app.use(bodyparser.json())
+    app.use(express.static(__dirname + '/public'))
+    app.use('/css' , express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+    app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
     //handlebars
     app.engine('handlebars', handlebars({defaultLayout : 'main'}))
     app.set('view engine', 'handlebars')
     //mongoose
 
 //routes
+app.get('/', (req,res) => {
+    res.render('home')
+})
 const test = require('./routes/test')
 app.use('/test', test)
 
 //start
 app.listen(PORT, ( ) => {
-    console.log('server running on http://localhost:' + PORT + ' .')
+    console.log('server running on http://localhost:' + PORT)
 })
